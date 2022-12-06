312dcecf131b900cead88d61ecdb7a3f064f2f9e
Naveen <solidauto202@gmail.com>
2022-12-06 10:31:08 +0530

    waste

--- Sol.java
+++ Sol.java
@@ -2,7 +2,9 @@ public class Sol {
     public static void main(String[] args) {
         int n=5;
         for(int i=1;i<=10;i++){
+            System.out.println("this is 5 table");
             System.out.println(i*n);
+
         }
     }
 }
